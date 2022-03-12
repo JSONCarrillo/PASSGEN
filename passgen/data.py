74d384f1de03c488367554962f40e98688ce3497
jayce <jaycelinux@gmail.com>
2022-03-11 21:48:13 -0700

    add database functionality

---
+++ passgen/data.py
@@ -0,0 +1,57 @@
+import sqlite3 as sq
+
+db = 'data.db'
+
+def connect():
+    connection = sq.connect(db)
+    con = connection.cursor()
+    con.execute(
+        """
+            CREATE TABLE IF NOT EXISTS data (
+                user text,
+                site text,
+                password text primary key
+            )
+        """
+    )
+
+    connection.commit()
+    connection.close()
+
+def enter(user, site, passwd):
+    connection = sq.connect(db)
+    con = connection.cursor()
+    con.execute("INSERT INTO data VALUES(?,?,?)", (user, site, passwd))
+    connection.commit()
+    connection.close()
+
+def show():
+    connection = sq.connect(db)
+    con = connection.cursor()
+    con.execute("SELECT * FROM data")
+    fetch = con.fetchall()
+    connection.commit()
+    connection.close()
+    return fetch
+
+def Delete(password):
+    connection = sq.connect(db)
+    con = connection.cursor()
+    con.execute("DELETE FROM data WHERE password=(?)", (password,))
+    connection.commit()
+    connection.close()
+
+def edit(user, site, passwd):
+    connection = sq.connect(db)
+    con = connection.cursor()
+    con.execute("UPDATE data SET site=(?), user=(?) WHERE password=(?)", (site, user, passwd))
+    connection.commit()
+    connection.close()
+
+def checkIfEmpty():
+    if len(show()) == 0:
+        return True
+    else:
+        return False
+
+connect()
--- passgen/passgen.py
+++ passgen/passgen.py
@@ -1,8 +1,11 @@
-import random
 #imports ttk as well as messagebox
+import random
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 from ttkbootstrap import *
 import webbrowser
+import data
 
 class Window:
     # list of all characters
@@ -20,58 +23,102 @@ class Window:
     def genPassword(self):
         password = ''
         i = 0
+        try:
+            passwd_length = int(self.length.get())
+            while i < passwd_length:
+                char_choice = random.randint(0,3)
 
-        while i < pass_length:
-            char_choice = random.randint(0,3)
+                if char_choice == 0:
+                    password += self.lower_case[random.randint(0, len(self.lower_case) - 1)]
 
-            if char_choice == 0:
-                password += self.lower_case[random.randint(0, len(self.lower_case) - 1)]
+                elif char_choice == 1:
+                    password += str(random.randint(0,9))
 
-            elif char_choice == 1:
-                password += str(random.randint(0,9))
+                elif char_choice == 2:
+                    password += self.upper_case[random.randint(0, len(self.upper_case) - 1)]
 
-            elif char_choice == 2:
-                password += self.upper_case[random.randint(0, len(self.upper_case) - 1)]
+                else: 
+                    password += self.special_char[random.randint(0, len(self.special_char) - 1)]
 
-            else: 
-                password += self.special_char[random.randint(0, len(self.special_char) - 1)]
+                i+= 1
+        except:
+            messagebox.showerror('Attention!', 'Password Length not set')
 
-            i+= 1
-
-        return password
+        return self.passwd.set(password)
 
     def help(self):
-        webbrowser.open('help.html')
-    
-    def view(self):
-        pass
-
-    def update(self):
-        pass
-
-    def save(self):
-        pass
-
-    def delete(self):
-        pass
-    
-    def select(self):
-        pass
-
-    def clearTable(self):
-        pass
-
-    def clearInputs(self):
-        pass
+        webbrowser.open('https://github.com/JSONCarrillo/PASSGEN')
     
     def refresh(self):
-        pass
+        self.clearTable()
+        self.clearInputs()
+        self.view()
+
+    def view(self):
+
+        self.clearTable()
+        # method shows all data from the database
+        if data.checkIfEmpty() is False:
+            for row in data.show():
+                self.tree.insert(
+                    parent='', text='', index='end',
+                    values=(row[2], row[0], row[1])
+                )
+        else:
+            messagebox.showerror('Whoops!', 'Looks like the database is empty')
+
+    def update(self):
+        selected_row = self.tree.focus()
+        val = self.tree.item(selected_row, 'value')
+        data.edit(self.username.get(), self.website.get(), self.passwd.get())
+        self.refresh()
+
+    def save(self):
+        try:
+            data.enter(self.username.get(), self.passwd.get(), self.website.get())
+            self.tree.insert(
+                parent='', index='end', text='',
+                values=(self.username.get(), self.website.get(), self.passwd.get())
+                )
+            self.refresh()
+        except:
+            messagebox.showerror('Whoops!', 'Looks like that password already exists, try again!')
+
+    def delete(self):
+        try:
+            selected_row = self.tree.focus()
+            value = self.tree.item(selected_row, 'value')
+            data.Delete(value[2])
+            self.refresh()
+        except:
+            messagebox.showerror('Whoops!', 'No row selected')
+
+    def select(self, event):
+        self.passwd.set('')
+        self.username.set('')
+        self.website.set('')
+        selected_row = self.tree.focus()
+        value = self.tree.item(selected_row, 'value')
+        self.username.set(value[1])
+        self.passwd.set(value[2])
+        self.website.set(value[0])
+
+
+    def clearTable(self):
+        for row in self.tree.get_children():
+            self.tree.delete(row)
+
+    def clearInputs(self):
+        self.username.set('')
+        self.passwd.set('')
+        self.website.set('')
     
     def insert(self):
         pass
     
+    
     def rightClickMenu(self, event):
-        self.menu.tk_popup(event.x_root+12, event.y_root)
+        self.menu.tk_popup(event.x_root+1, event.y_root+1)
 
     def __init__(self, root, title, winsize):
         self.root = root
@@ -129,7 +176,7 @@ class Window:
         ttk.Button(
         self.root, text='Delete', style='danger.TButton', width=20, padding=5, 
         command=self.delete
-        ).grid(row=3, column=0)
+        ).grid(row=5, column=1)
 
         ttk.Button(
         self.root, text='View All Passwords', width=20, padding=5, 
@@ -137,8 +184,8 @@ class Window:
         ).grid(row=3, column=0)
         
         ttk.Button(
-        self.root, text='Update Password', width=20, padding=5, 
-        command=self.view
+        self.root, text='Update Site and User', width=20, padding=5, 
+        command=self.update
         ).grid(row=3, column=1)
 
         # Table to display usernames, passwords, and sites
@@ -164,7 +211,7 @@ class Window:
         self.menu = Menu(self.root, tearoff=False)
         self.menu.add_command(label='Refresh', command=self.refresh)
         self.menu.add_command(label='Update', command=self.update)
-        self.menu.add_command(label='Insert', command=self.insert)
+        self.menu.add_command(label='Save', command=self.save)
         self.menu.add_separator()
 
         self.menu.add_command(label='Show All', command=self.view)
